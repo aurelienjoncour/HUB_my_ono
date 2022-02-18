0014d5ea7b787fa943c63f63878f737602118746
Aurélien Joncour <aurelien.joncour@epitech.eu>
2022-02-18 10:31:38 +0100

    fix(client/printCards.py) add printCards with Cards class

--- client/printCards.py
+++ client/printCards.py
@@ -7,13 +7,11 @@ import pygame
 def create_card(filepath):
     return pygame.image.load(filepath)
 
-def print_deck(filepathList, screen):
+def print_deck(filepathList, cards):
     y = 50
     x = 10
     cardList = []
     for i in range(len(filepathList)):
-        cardList.append(create_card(f"asset/cards/{filepathList[i]}"))
-        screen.blit(cardList[i], (x,y))
-        print(filepathList[i])
+        cards.display(filepathList[i], (x, y))
         x = x + 40
     return cardList
