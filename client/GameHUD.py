d08dd924cce8100d3d0b6d31055f4942a06ca16c
Esteban Hulin <esteban.hulin@epitech.eu>
2022-02-18 18:08:09 +0100

    ADD some HUD funtions

--- client/GameHUD.py
+++ client/GameHUD.py
@@ -3,20 +3,29 @@ import pygame
 
 class GameHUD:
     def __init__(self, screen) -> None:
+        self.card_w = 64
+        self.card_h = 96
         self.cards = Cards(screen)
+        self.infoObject = pygame.display.Info()
         self.screen = screen
-        self.font = pygame.font.SysFont('Arial', 25)
+        self.font_name = pygame.font.SysFont('Arial', 25)
+        self.font_nb_card = pygame.font.SysFont('Arial', 50)
         pygame.display.set_caption('Box Test')
 
     def card_indicator(self):
-        #TODO: display 2 overlapping cards icons and a number
+        self.screen.blit(self.font_nb_card.render('7', True, (255,255,255)), (10, 150))
         print("card indicator")
 
     def player_name(self):
-        self.screen.blit(self.font.render('Jaajeur', True, (255,255,255)), (20, 150))
+        self.screen.blit(self.font_name.render('Jaajeur', True, (255,255,255)), (50, 150))
         print("player name")
 
-    def opponent_card_deck(self):
+    def opponent_card_deck(self, cardList):
+        y = self.infoObject.current_h - self.card_h -50
+        x = 10
+        for card in cardList:
+            self.cards.display("uno_back.png", (x, y))
+            x = x + 40
         #TODO: display hidden deck
         print("card hidden")
 
@@ -28,7 +37,7 @@ class GameHUD:
             x = x + 40
 
     def top_stack_card(self, card):
-        self.cards.display(card.filepath, (500, 500))
+        self.cards.display(card.filepath, (self.infoObject.current_w / 2 - self.card_w, self.infoObject.current_h / 2 - self.card_h))
 
     def opponent(self):
         #TODO: display all components of the oponent
@@ -39,7 +48,7 @@ class GameHUD:
     def player(self, cardList):
         #TODO: display actual player
         self.player_name()
-        #self.card_indicator()
+        self.card_indicator()
         self.player_card_deck(cardList)
 
     def draw_arrow(self):
