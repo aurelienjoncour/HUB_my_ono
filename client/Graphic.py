f399a6b94d179223c629e64c42578e452200de8c
Aurélien Joncour <aurelien.joncour@epitech.eu>
2022-02-18 10:37:44 +0100

    feat(client/) call network, cards, and menu class

--- client/Graphic.py
+++ client/Graphic.py
@@ -2,6 +2,7 @@
 
 import pygame
 from printCards import print_deck
+from Cards import Cards
 
 class Graphic:
 
@@ -9,28 +10,24 @@ class Graphic:
 	window_height = 720
 	isrunning = True
 	screen = None
+	cards = None
 	background_image = None
 	flags = pygame.RESIZABLE
 	clock = pygame.time.Clock()
-	screens = 0
 
-	def __init__(self) -> None:
+	def __init__(self, screen) -> None:
 		pygame.init()
-		infoObject = pygame.display.Info()
-		self.screen = pygame.display.set_mode([infoObject.current_w, infoObject.current_h], pygame.RESIZABLE)
+		self.screen = screen
+		self.cards = Cards(self.screen)
 		self.background_image = pygame.image.load("asset/my_uno_bg.png").convert()
 
 	def mainLoop(self):
 		test_list = ["uno_y_4.png","uno_r_7.png","uno_g_4.png"]
 		while self.isrunning:
 			self.screen.fill((255, 255, 255))
+			self.screen.blit(self.background_image, [0, 0])
+			print_deck(test_list, self.cards)
+			pygame.display.flip()
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					self.isrunning = False
-			if self.screens == 0:
-				self.screen.blit(self.background_image, [0, 0])
-				print_deck(test_list,self.screen)
-				pygame.display.flip()
-
-			elif self.screens == 1:
-				pass
--- client/main.py
+++ client/main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 
+import pygame
+from MainMenu import MainMenu
 from Graphic import Graphic
 from Network import Network
 
-#network = Network("", 8080)
-#ret = network.getRet()
-#print(ret)
+pygame.init()
 
-Main = Graphic()
-Main.mainLoop()
+infoObject = pygame.display.Info()
+screen = pygame.display.set_mode([infoObject.current_w, infoObject.current_h], pygame.RESIZABLE)
+
+menu = MainMenu(screen)
+Main = Graphic(screen)
+
+menu.menuLoop()
+
+if not menu.should_exit:
+    network = Network(menu.ip_address, 8080)
+    ret = network.getRet()
+    Main.mainLoop()
